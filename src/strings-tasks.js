ba7174f7e9024ffdaff6e5448d78401638ae4a73
Sergey Kruglov <serega-kolpino@inbox.ru>
2023-11-27 00:01:13 +0300

    feat: compleat 1 tsk

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -376,8 +376,10 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence
+    .split(' ')
+    .reduce((a, current) => (a.length >= current.length ? a : current), '');
 }
 
 /**
